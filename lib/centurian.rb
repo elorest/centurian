e50de9b561e15144d328f7e233181295b8eecb9e
Isaac Sloan <isaac@isaacsloan.com>
2015-07-17 17:11:23 -0600

    we're safe to deploy now. for reals

--- lib/centurian.rb
+++ lib/centurian.rb
@@ -1,11 +1,12 @@
 require "centurian/version"
 
 module Centurian
-  ROMAN = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X:10, IX: 9, V:5, IV: 4, I: 1}
+  ROMAN = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1}
 
   def to_roman
+    number = self
     ROMAN.reduce("") do |roman, (k, v)|
-      count, number = (number || self).divmod(v)
+      count, number = number.divmod(v)
       roman << (k.to_s * count)
     end
   end
